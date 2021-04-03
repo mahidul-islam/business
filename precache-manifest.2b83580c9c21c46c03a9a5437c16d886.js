b15a2f6cb459f449e761161d905ee1864aa6dc9a
mahidul-islam <mizihan84@gmail.com>
2021-04-03 11:32:56 +0600

    Updates

---
+++ precache-manifest.2b83580c9c21c46c03a9a5437c16d886.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/business/favicon.ico"
+  },
+  {
+    "revision": "70cc63069a7e6afca7be4acf91768553",
+    "url": "/business/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/business/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/business/manifest.json"
+  },
+  {
+    "revision": "7cec5a6538a5e3eb861f",
+    "url": "/business/static/css/main.0af5c45f.chunk.css"
+  },
+  {
+    "revision": "7cec5a6538a5e3eb861f",
+    "url": "/business/static/js/main.7cec5a65.chunk.js"
+  },
+  {
+    "revision": "30970f2b7b5fadb3e3de",
+    "url": "/business/static/js/runtime~main.30970f2b.js"
+  },
+  {
+    "revision": "0b49ec49a892799f59f3",
+    "url": "/business/static/js/vendors~main.0b49ec49.chunk.js"
+  }
+]);
